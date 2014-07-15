db349f58c871a3cd89acb015e995d30e00e557c4
ht <ht@5883acad-0c5e-4a7b-bdb2-e9f4f061bb49>
2014-07-15 14:16:21 +0000

    copy to latest seal.NET version 4.5 (r611)

---
+++ latest/code/Saml2AssertionSerializer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Selectors;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel.Security;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+#if NET35
+using Microsoft.IdentityModel.Tokens.Saml2;
+using Microsoft.IdentityModel.Tokens;
+#endif
+
+namespace dk.nsi.seal
+{
+    
+    internal class Saml2AssertionSerializer : Saml2SecurityTokenHandler
+    {
+        public Saml2AssertionSerializer()
+        {
+            Configuration = new SecurityTokenHandlerConfiguration()
+            {
+                IssuerTokenResolver = new Saml2IssuerTokenResolver()
+            };
+        }
+
+        public Saml2AssertionSerializer(SamlSecurityTokenRequirement samlSecurityTokenRequirement): base(samlSecurityTokenRequirement)
+        {
+            Configuration = new SecurityTokenHandlerConfiguration()
+            {
+                IssuerTokenResolver = new Saml2IssuerTokenResolver()
+            };
+        }
+
+        public Saml2Assertion ReadSaml2Assertion(XmlReader rd)
+        {
+            return base.ReadAssertion(rd);
+        }
+
+        public void WriteSaml2Assertion(XmlWriter wr, Saml2Assertion sa)
+        {
+            base.WriteAssertion(wr, sa);
+        }
+    }
+    
+    class Saml2IssuerTokenResolver : IssuerTokenResolver
+    {
+        protected override bool TryResolveSecurityKeyCore(SecurityKeyIdentifierClause keyIdentifierClause, out SecurityKey key)
+        {
+            key = null;
+            var kn = keyIdentifierClause as X509RawDataKeyIdentifierClause;
+            if (kn == null) return false;
+            var cert = new X509Certificate2(kn.GetX509RawData());
+            if (cert == null) return false;
+            key = new X509AsymmetricSecurityKey(cert);
+            return key != null;
+        }
+    }
+}
---
+++ latest/code/SecurityTokens.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+#if NET35
+using Microsoft.IdentityModel.Tokens.Saml2;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens;
+#else
+using System.IdentityModel.Tokens;
+#endif
+
+
+
+namespace dk.nsi.seal
+{
+    public class SealSecurityToken : SecurityToken
+    {
+        public SealCard sealCard;
+        public SealSecurityToken(XElement assertion)
+        {
+            this.sealCard = new SealCard { Xassertion = assertion };
+        }
+
+        public SealSecurityToken(SealCard sealCard)
+        {
+            this.sealCard = sealCard;
+        }
+
+        public override string Id
+        {
+            get { return sealCard.Id; }
+        }
+
+        public override System.Collections.ObjectModel.ReadOnlyCollection<SecurityKey> SecurityKeys
+        {
+            get { return null; }
+        }
+
+        public override DateTime ValidFrom
+        {
+            get
+            {
+                return sealCard.ValidFrom;
+            }
+        }
+
+        public override DateTime ValidTo
+        {
+            get
+            {
+                return sealCard.ValidTo;
+            }
+        }
+    }
+
+    public class SealSecurityTokenHandler : SecurityTokenHandler
+    {
+        public override string[] GetTokenTypeIdentifiers()
+        {
+            return new string[] { Constants.SealSecurityTokenHandlerId };
+        }
+
+        public override Type TokenType
+        {
+            get { return typeof(SealSecurityToken); }
+        }
+
+        public override bool CanWriteToken
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override void WriteToken(XmlWriter writer, SecurityToken token)
+        {
+            var t = token as SealSecurityToken;
+            t.sealCard.Xassertion.WriteTo(writer);
+        }
+    }
+
+
+    class Saml2SecurityToken2 : Saml2SecurityToken
+    {
+        public Saml2Assertion health;
+        public Saml2SecurityToken2(Saml2Assertion org, Saml2Assertion health)
+            : base(org)
+        {
+            this.health = health;
+        }
+    }
+
+    class Saml2SecurityToken2TokenHandler : Saml2SecurityTokenHandler
+    {
+        Saml2AssertionSerializer ser = new Saml2AssertionSerializer();
+
+        public override void WriteToken(XmlWriter writer, SecurityToken token)
+        {
+            var t = token as Saml2SecurityToken2;
+            base.WriteToken(writer, token);
+            ser.WriteSaml2Assertion(writer, t.health);
+        }
+
+        public override Type TokenType
+        {
+            get { return typeof(Saml2SecurityToken2); }
+        }
+    }
+
+
+    class SealSaml2SecurityToken : SecurityToken
+    {
+        public XElement assertion;
+        public SealSaml2SecurityToken(XElement assertion)
+        {
+            this.assertion = assertion;
+        }
+
+        public override string Id
+        {
+            get { return "IDCard"; }
+        }
+
+        public override System.Collections.ObjectModel.ReadOnlyCollection<SecurityKey> SecurityKeys
+        {
+            get { return null; }
+        }
+
+        public override DateTime ValidFrom
+        {
+            get
+            {
+                if (assertion == null) return DateTime.MaxValue;
+                return DateTime.Parse(assertion.Element(ns.xsaml + "Conditions").Attribute("NotBefore").Value);
+            }
+        }
+
+        public override DateTime ValidTo
+        {
+            get
+            {
+                if (assertion == null) return DateTime.MinValue;
+                return DateTime.Parse(assertion.Element(ns.xsaml + "Conditions").Attribute("NotOnOrAfter").Value);
+            }
+        }
+    }
+
+    
+    public class SealSaml2SecurityTokenHandler : SecurityTokenHandler
+    {
+        public override string[] GetTokenTypeIdentifiers()
+        {
+            return new string[] { Constants.DGWSSecurityTokenHandlerId };
+        }
+
+        public override Type TokenType
+        {
+            get { return typeof(SealSaml2SecurityToken); }
+        }
+
+        public override bool CanWriteToken
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override void WriteToken(XmlWriter writer, SecurityToken token)
+        {
+            var t = token as SealSaml2SecurityToken;
+            if (t.assertion != null)
+            {
+                t.assertion.WriteTo(writer);
+            }
+        }
+    }
+
+
+    class SosiGWCardSecurityToken : SecurityToken
+    {
+        public XElement assertion;
+        public SosiGWCardSecurityToken(XElement assertion)
+        {
+            this.assertion = assertion;
+        }
+
+        public override string Id
+        {
+            get { return "IDCard"; }
+        }
+
+        public override System.Collections.ObjectModel.ReadOnlyCollection<SecurityKey> SecurityKeys
+        {
+            get { return null; }
+        }
+
+        public override DateTime ValidFrom
+        {
+            get
+            {
+                return DateTime.Parse(assertion.Element(ns.xsaml + "Conditions").Attribute("NotBefore").Value);
+            }
+        }
+
+        public override DateTime ValidTo
+        {
+            get
+            {
+                return DateTime.Parse(assertion.Element(ns.xsaml + "Conditions").Attribute("NotOnOrAfter").Value);
+            }
+        }
+    }
+
+    public class SosiGWCardTokenHandler : SecurityTokenHandler
+    {
+        public override string[] GetTokenTypeIdentifiers()
+        {
+            return new string[] { Constants.SosiGWSecurityTokenHandlerId };
+        }
+
+        public override Type TokenType
+        {
+            get { return typeof(SosiGWCardSecurityToken); }
+        }
+
+        public override bool CanWriteToken
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override void WriteToken(XmlWriter writer, SecurityToken token)
+        {
+            writer.WriteStartElement("SecurityTokenReference", ns.wsse);
+                writer.WriteAttributeString("URI","#IDCard" );
+            writer.WriteEndElement();
+        }
+    }
+
+
+}
